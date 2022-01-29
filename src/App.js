344fb5909b5d768b15f04d17626e2c2216e4cfa3
Mohammad Danish <mohddanish8299@gmail.com>
2022-01-29 14:46:32 +0530

    redirect / => /home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/home';
 import Profile from './pages/profile';
 import Navbar from './components/navbar/navbar.component';
@@ -6,11 +6,12 @@ import Navbar from './components/navbar/navbar.component';
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        {/* Fallback for default URL, Redirect to /home */}
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </Router>
   );
