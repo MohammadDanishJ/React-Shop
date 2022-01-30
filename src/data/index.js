5a76e1c48545070f30bfe58ce3e9230433a28fbd
Mohammad Danish <mohddanish8299@gmail.com>
2022-01-30 12:09:26 +0530

    use rate as int

--- src/data/index.js
+++ src/data/index.js
@@ -14,21 +14,21 @@ export const shop = [
         id: 11,
         name: 'Shop 1',
         location: 'location 1',
-        rate: '110'
+        rate: 110
     }, {
         id: 12,
         name: 'Shop 2',
         location: 'location 2',
-        rate: '120'
+        rate: 120
     }, {
         id: 13,
         name: 'Shop 3',
         location: 'location 3',
-        rate: '130'
+        rate: 105
     }, {
         id: 14,
         name: 'Shop 4',
         location: 'location 4',
-        rate: '140'
+        rate: 140
     },
 ]
