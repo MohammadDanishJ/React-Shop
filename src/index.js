cf1955218cf6c119ea0421435b5032433a8b05e8
Mohammad Danish <mohddanish8299@gmail.com>
2022-02-02 13:35:54 +0530

    replaced sw with reacts own sw

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './styles/base.scss';
 import App from './App';
-import swDev from './swDev';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-swDev();
+serviceWorkerRegistration.register();
--- src/swDev.js
+++
@@ -1,8 +0,0 @@
-const swDev = () => {
-    let swUrl = `${process.env.PUBLIC_URL}/sw.js`
-    navigator.serviceWorker.register(swUrl).then((res)=>{
-        console.log('respone', res)
-    })
-};
-
-export default swDev;
