6b0269d3e394f00cf2ff4cba1f95c8f85bfde0d6
Mohammad Danish <mohddanish8299@gmail.com>
2022-02-02 11:51:27 +0530

    update sw [added activate event]

--- public/sw.js
+++ public/sw.js
@@ -30,7 +30,7 @@ self.addEventListener('install', event => {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(function (cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
@@ -41,29 +41,26 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
+      })
   );
 });
 
 // // Update a service worker
-// self.addEventListener('activate', event => {
-//   var cacheWhitelist = ['pwa-task-manager'];
-//   event.waitUntil(
-//     caches.keys().then(cacheNames => {
-//       return Promise.all(
-//         cacheNames.map(cacheName => {
-//           if (cacheWhitelist.indexOf(cacheName) === -1) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName === CACHE_NAME) { return; }
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
