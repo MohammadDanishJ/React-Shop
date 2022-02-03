3bad099d7053568b4afe50f84a500d36d8c34f07
Mohammad Danish <mohddanish8299@gmail.com>
2022-02-03 20:18:38 +0530

    use local vars instead of env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,9 +1,20 @@
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
-};
+    apiKey: "AIzaSyAen4PIYfAXBQXir4NsxerFB0qAbXmYsc8",
+    authDomain: "egshop11.firebaseapp.com",
+    projectId: "egshop11",
+    storageBucket: "egshop11.appspot.com",
+    messagingSenderId: "1031847083602",
+    appId: "1:1031847083602:web:08f5d0103ffc5b553da98b",
+    measurementId: "G-NZY43KMH7E"
+  };
+
+
+// export const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
+//   };
