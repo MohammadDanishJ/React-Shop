795e3688e7bcf3fdcb96922eab6ba39261a120b0
Mohammad Danish <mohddanish8299@gmail.com>
2022-02-10 22:57:40 +0530

    added better loading states for offline||no-data

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -5,6 +5,7 @@ import Container from '../../components/container/container.component';
 import tabTitle from '../page';
 import { db } from "../../firebase/firebaseUtils";
 import { collection, getDocs } from 'firebase/firestore';
+import { nullShop } from '../../data';
 
 function findShop(arr, value) {
     const element = arr.find((el) => {
@@ -17,11 +18,24 @@ const Shop = () => {
     const { sname } = useParams();
 
     const [shop, setShop] = useState([]);
+    const [isLoading, setisLoading] = useState(true);
+
     const shopRef = collection(db, 'shop');
     useEffect(() => {
         const getShop = async () => {
-            const data = await getDocs(shopRef);
-            setShop(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            getDocs(shopRef).then((data => {
+                if (!data.empty) {
+                    setShop(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+                    setisLoading(false)
+                } else {
+                    setShop(nullShop)
+                    setisLoading(false)
+                }
+            })).catch(e => {
+                setShop(nullShop)
+                setisLoading(false)
+                console.log(e)
+            });
         }
 
         getShop()
@@ -35,29 +49,35 @@ const Shop = () => {
 
     return (
         <Container>
-            {sname ? (
-                // if user visits with id in URL
-                // path: /shop/id
-                <>
-                    {shopFound ? (
-                        // if shop found with GET id
-                        <div className='fl fl-c fl-d-cl w100 h100'>
-                            <div>Id {Shop.id}</div>
-                            <div>Name {Shop.name}</div>
-                            <div>Location {Shop.location}</div>
-                            <div>Rate {Shop.rate}</div>
-                        </div>
+            {
+                !isLoading ?
+
+                    sname ? (
+                        // if user visits with id in URL
+                        // path: /shop/id
+                        <>
+                            {
+                                shopFound ? (
+                                    // if shop found with GET id
+                                    <div className='fl fl-c fl-d-cl w100 h100' >
+                                        <div>Id {Shop.id}</div>
+                                        <div>Name {Shop.name}</div>
+                                        <div>Location {Shop.location}</div>
+                                        <div>Rate {Shop.rate}</div>
+                                    </div>
+                                ) : (
+                                    // if shop not found
+                                    <div className='fl fl-c w100 h100'>Shop Not Found </div>
+                                )
+                            }
+                        </>
                     ) : (
-                        // if shop not found
-                        <div className='fl fl-c w100 h100'>Shop Not Found </div>
-                    )}
-                </>
-            ) : (
-                // if user visits without id in URL
-                // path: /shop
-                <div className='fl fl-c w100 h100'>Default Shop</div>
-            )}
-        </Container>
+                        // if user visits without id in URL
+                        // path: /shop
+                        <div className='fl fl-c w100 h100'>Default Shop</div>
+                    ) : <div className='fl fl-c w100 h100vh'>Loading...</div>}
+
+        </Container >
     );
 };
 
